7dd4b3f9c3c7e82f55711fcbc391a98e18376d44
GiovanniCarbinatti <83474533+GiovanniCarbinatti@users.noreply.github.com>
2024-11-19 07:27:22 -0300

    update header

--- download.py
+++ download.py
@@ -35,6 +35,7 @@ if url:
                         'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s'),
                         'http_headers': {
                                         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                                        'Accept-Language': 'en-US,en;q=0.9',
                                         },
                     }
 
